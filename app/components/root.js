b6cfd83a655a71f58153fee5a414092e2a9efe7b
Rashmi Prakash <rashmiprakash@Rashmis-MacBook-Pro.local>
2019-02-26 15:00:12 -0500

    initital commit of app folder

---
+++ app/components/root.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Root = () => {
+  return (
+    <div>
+      <nav>
+        Welcome!
+      </nav>
+      <main>
+        <h1> This is my Library </h1>
+        <p> Lets start with some Routes </p>
+      </main>
+    </div>
+  )
+}
+export default Root
