185f86bf83a8c077c87404b99ee01c7b363f269f
Rashmi Prakash <rashmiprakash21@hotmail.com>
2019-02-28 17:40:48 -0500

    started the react and redux part

---
+++ app/reducers/bookReducer.js
@@ -0,0 +1,8 @@
+const initialState = {};
+
+export const bookReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,15 +1,21 @@
 // `combineReducers` is not currently being used...but it should!
 // When you're ready to use it, un-comment the line below!
 
-// import {combineReducers} from 'redux'
+import { combineReducers } from 'redux'
+import { categoryReducer } from './categoryReducer'
+import { bookReducer} from './bookReducer'
 
-const initialState = {};
+const rootReducer = combineReducers({
+  category: categoryReducer,
+  book: bookReducer
+})
+// const initialState = {};
 
-const rootReducer = (state = initialState, action) => {
-  switch (action.type) {
-    default:
-      return state;
-  }
-};
+// const rootReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     default:
+//       return state;
+//   }
+// };
 
 export default rootReducer;
