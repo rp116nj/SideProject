8bfcc37df03739230e0c4d3ea33cd3f242c89fda
Rashmi Prakash <rashmiprakash21@hotmail.com>
2019-02-28 17:51:33 -0500

    reducers modified

--- app/reducers/bookReducer.js
+++ app/reducers/bookReducer.js
@@ -1,7 +1,18 @@
-const initialState = {};
+const initialState = {
+  books: []
+};
+// Action Type
+const gotAllBooks = 'GET_ALL_BOOKS'
 
+//Action Creator
+export const getAllBooks = (books) => ({
+  type: gotAllBooks,
+  books
+})
 export const bookReducer = (state = initialState, action) => {
   switch (action.type) {
+    case gotAllBooks:
+      return {...state, books: action.books}
     default:
       return state;
   }
--- app/reducers/categoryReducer.js
+++ app/reducers/categoryReducer.js
@@ -1,7 +1,18 @@
-const initialState = {};
-
+const initialState = {
+  categories: []
+};
+// Action Type
+const gotAllCategories = 'GET_ALL_CATEGORIES'
+ 
+//Action Creator
+export const getAllCategories = (categories) => ({
+  type: getAllCategories,
+  categories
+})
 export const categoryReducer = (state = initialState, action) => {
   switch (action.type) {
+    case gotAllCategories:
+      return {...state, categories: action.categories}
     default:
       return state;
   }
