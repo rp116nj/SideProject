a23aac8ba93a62b5e44180c0b8f79e269b74731a
Rashmi Prakash <rashmiprakash21@hotmail.com>
2019-03-07 10:14:42 -0500

    book route modified

--- server/api/bookRoutes.js
+++ server/api/bookRoutes.js
@@ -1,16 +1,24 @@
-const router = require('express').Router()
-const books = require('../db/models/Books')
+const router = require('express').Router();
+const books = require('../db/models/Books');
 
 //GET all Books ('/api/books')
 
 router.get('/', async (req, res, next) => {
   try {
-    const allBooks = await books.findAll()
-    res.json(allBooks)
+    const allBooks = await books.findAll();
+    res.json(allBooks);
+  } catch (error) {
+    next(error);
   }
-  catch (error) {
-    next(error)
-  }
-})
+});
 
-module.exports = router
+router.get('/:id', async (req, res, next) => {
+  try {
+    const book = await books.findByPk(req.params.id);
+    res.json(book);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
